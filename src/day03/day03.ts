8d5dce5c36a4a637acee4e56b7953111a3bca223
Daniel Schoenbach <10995699+MagicShoebox@users.noreply.github.com>
2023-12-04 00:04:32 -0500

    Day 3

---
+++ src/day03/day03.ts
@@ -0,0 +1,86 @@
+import { main } from "../main"
+import { partition, range } from "../util"
+
+type Part = {
+    y: number,
+    x: number,
+    length: number
+}
+
+function solver(input: string[]) {
+    const numbers = []
+    const symbols = new Set()
+    for (let y = 0; y < input.length; y++) {
+        let start = null
+        for (let x = 0; x < input[y].length; x++) {
+            if (/[0-9]/.test(input[y][x])) {
+                if (start != null)
+                    continue
+                start = x
+            } else {
+                if (input[y][x] != ".")
+                    symbols.add(JSON.stringify([y, x]))
+                if (start == null)
+                    continue
+                numbers.push({ y, x: start, length: x - start })
+                start = null
+            }
+        }
+        if (start != null)
+            numbers.push({ y, x: start, length: input[y].length - start })
+    }
+
+    const neighborFn = neighbors(input.length, input[0].length)
+    const hasSymbolNeighbor = (n: Part) =>
+        [...neighborFn(n)].some(({ y, x }) =>
+            symbols.has(JSON.stringify([y, x])))
+    const partNo = ({ y, x, length }: Part) => parseInt(input[y].substring(x, x + length))
+    const part1 = numbers
+        .filter(hasSymbolNeighbor)
+        .map(partNo)
+        .sum()
+        .toString()
+
+    const gearParts = partition(
+        ({ key }) => key,
+        numbers.flatMap(number =>
+            [...neighborFn(number)]
+                .filter(({ y, x }) => symbols.has(JSON.stringify([y, x])) && input[y][x] == "*")
+                .map(({ y, x }) => ({ key: JSON.stringify([y, x]), part: partNo(number) }))))
+    const part2 =
+        [...gearParts.values()]
+            .filter(ns => ns.length == 2)
+            .map(ns => ns.map(n => n.part).prod())
+            .sum()
+            .toString()
+
+    return [
+        part1,
+        part2
+    ]
+}
+
+function neighbors(height: number, width: number) {
+    return function* ({ y, x, length }: Part) {
+        if (x > 0) {
+            if (y > 0)
+                yield { y: y - 1, x: x - 1 }
+            yield { y, x: x - 1 }
+            if (y + 1 < height)
+                yield { y: y + 1, x: x - 1 }
+        }
+        if (x + length < width) {
+            if (y > 0)
+                yield { y: y - 1, x: x + length }
+            yield { y, x: x + length }
+            if (y + 1 < height)
+                yield { y: y + 1, x: x + length }
+        }
+        if (y > 0)
+            yield* [...range(x, x + length)].map(x => ({ y: y - 1, x }))
+        if (y + 1 < height)
+            yield* [...range(x, x + length)].map(x => ({ y: y + 1, x }))
+    }
+}
+
+main(solver)
--- src/util.ts
+++ src/util.ts
@@ -7,6 +7,31 @@ export function assertRequired<T extends object>(obj: T): asserts obj is
     });
 }
 
+export function* range(start: number, stop?: number, step?: number) {
+    if (stop === undefined) {
+        stop = start
+        start = 0
+    }
+    if (step === undefined)
+        step = 1
+    if (step <= 0)
+        throw new Error("Not implemented")
+    for (let i = start; i < stop; i += step)
+        yield i
+}
+
+// https://stackoverflow.com/a/38863774
+export function partition<T, K>(f: (arg0: T) => K, xs: T[]) {
+    const append = function (ys: T[] = [], y: T) {
+        ys.push(y)
+        return ys
+    }
+    return xs.reduce((m, x) => {
+        let v = f(x)
+        return m.set(v, append(m.get(v), x))
+    }, new Map<K, T[]>())
+}
+
 declare global {
     interface Array<T> {
         sum(): number
